9435da174fe441f4ec552ee3d16a60b1658c53a0
panhaoyu <panhaoyu.china@outlook.com>
2023-09-16 19:00:01 +0800

    supports to dumps/loads with django model instance

--- django_q/signing.py
+++ django_q/signing.py
@@ -1,6 +1,8 @@
 """Package signing."""
 import pickle
 
+from django.db import models
+
 from django_q import core_signing as signing
 from django_q.conf import Conf
 
@@ -31,9 +33,38 @@ class PickleSerializer:
     """Simple wrapper around Pickle for signing.dumps and signing.loads."""
 
     @staticmethod
+    def _dump_model_instance(value_or_obj):
+        if isinstance(value_or_obj, models.Model):
+            return type(value_or_obj), value_or_obj.pk
+        else:
+            return value_or_obj
+
+    @staticmethod
+    def _load_model_instance(value_or_obj):
+        if isinstance(value_or_obj, tuple) and len(value_or_obj) == 2:
+            cls, pk = value_or_obj
+            if issubclass(cls, models.Model):
+                obj = cls(pk=pk)
+                obj.refresh_from_db()
+                return obj
+        else:
+            return value_or_obj
+
+    @staticmethod
     def dumps(obj) -> bytes:
+        if isinstance(obj, dict):
+            if isinstance(args := obj.get('args', None), tuple):
+                obj['args'] = tuple(PickleSerializer._dump_model_instance(v) for v in args)
+            if isinstance(kw := obj.get('kwargs', None), dict):
+                obj['kwargs'] = {k: PickleSerializer._dump_model_instance(v) for k, v in kw.items()}
         return pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL)
 
     @staticmethod
     def loads(data) -> any:
-        return pickle.loads(data)
+        data = pickle.loads(data)
+        if isinstance(data, dict):
+            if isinstance(args := data.get('args', None), tuple):
+                data['args'] = tuple(PickleSerializer._load_model_instance(v) for v in args)
+            if isinstance(kw := data.get('kwargs', None), dict):
+                data['kwargs'] = {k: PickleSerializer._load_model_instance(v) for k, v in kw.items()}
+        return data
